2abe3be53c16806e2833a27ea05ff6dacc390479
Anurag Kumar <akanuragkumar712@gmail.com>
2016-11-09 08:47:16 +0530

    Update linkedstack.c

--- linkedstack.c
+++ linkedstack.c
@@ -1,119 +1,180 @@
 #include <stdio.h>
 #include <stdlib.h>
-  
-struct node {
-    int data;
-    struct node *next;
-}*top;
  
-/*
-Initialize an empty stack
-*/
-void initialize() {
-    top = NULL;
-} 
+struct node
+{
+    int info;
+    struct node *ptr;
+}*top,*top1,*temp;
  
-/*
-Checks if Stack is empty or not
-*/
-int isEmpty() {
-    if (top == NULL) 
-        return 1;
-    else
-        return 0;
+int topelement();
+void push(int data);
+void pop();
+void empty();
+void display();
+void destroy();
+void stack_count();
+void create();
+ 
+int count = 0;
+ 
+void main()
+{
+    int no, ch, e;
+ 
+    printf("\n 1 - Push");
+    printf("\n 2 - Pop");
+    printf("\n 3 - Top");
+    printf("\n 4 - Empty");
+    printf("\n 5 - Exit");
+    printf("\n 6 - Dipslay");
+    printf("\n 7 - Stack Count");
+    printf("\n 8 - Destroy stack");
+ 
+    create();
+ 
+    while (1)
+    {
+        printf("\n Enter choice : ");
+        scanf("%d", &ch);
+ 
+        switch (ch)
+        {
+        case 1:
+            printf("Enter data : ");
+            scanf("%d", &no);
+            push(no);
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            if (top == NULL)
+                printf("No elements in stack");
+            else
+            {
+                e = topelement();
+                printf("\n Top element : %d", e);
+            }
+            break;
+        case 4:
+            empty();
+            break;
+        case 5:
+            exit(0);
+        case 6:
+            display();
+            break;
+        case 7:
+            stack_count();
+            break;
+        case 8:
+            destroy();
+            break;
+        default :
+            printf(" Wrong choice, Please enter correct choice  ");
+            break;
+        }
+    }
 }
  
-/*
-Returns the top element of Stack
-*/
-int peek() {
-    return top->data;
+/* Create empty stack */
+void create()
+{
+    top = NULL;
 }
  
 /* Count stack elements */
-int getStackSize(struct node *head){
-    /* Input Validation */
-    if (head == NULL) { 
-       printf("Error : Invalid stack pointer !!!\n");       
-       return;  
-    }
-      
-    int length = 0;
-    while(head != NULL){
-        head = head->next;
-        length++;
-    }
-    return length;
+void stack_count()
+{
+    printf("\n No. of elements in stack : %d", count);
 }
  
-/* 
-Push an Element in Stack 
-*/
-void push(int num) {
-    struct node *temp;
-    temp =(struct node *)malloc(1*sizeof(struct node));
-    temp->data = num;
-     
-    if (top == NULL) {
-        top = temp;
-        top->next = NULL;
-    } else {
-        temp->next = top;
+/* Push data into stack */
+void push(int data)
+{
+    if (top == NULL)
+    {
+        top =(struct node *)malloc(1*sizeof(struct node));
+        top->ptr = NULL;
+        top->info = data;
+    }
+    else
+    {
+        temp =(struct node *)malloc(1*sizeof(struct node));
+        temp->ptr = top;
+        temp->info = data;
         top = temp;
     }
+    count++;
 }
  
-/*
-Pop Operation: Removes Top Element of the Stack
-*/
-void pop() {
-    struct node *temp;
-    if (isEmpty(top)) {
-        printf("\nStack is Empty\n");
+/* Display stack elements */
+void display()
+{
+    top1 = top;
+ 
+    if (top1 == NULL)
+    {
+        printf("Stack is empty");
         return;
-    } else {
-        temp = top;
-        top = top->next;
-        printf("Removed  Element : %d\n", temp->data);   
-        free(temp); 
     }
+ 
+    while (top1 != NULL)
+    {
+        printf("%d ", top1->info);
+        top1 = top1->ptr;
+    }
+ }
+ 
+/* Pop Operation on stack */
+void pop()
+{
+    top1 = top;
+ 
+    if (top1 == NULL)
+    {
+        printf("\n Error : Trying to pop from empty stack");
+        return;
+    }
+    else
+        top1 = top1->ptr;
+    printf("\n Popped value : %d", top->info);
+    free(top);
+    top = top1;
+    count--;
 }
  
-/*
- Prints the linked list representation of a stack  
-*/
-void printStack(struct node *nodePtr) {
-  while (nodePtr != NULL) {
-     printf("%d", nodePtr->data);
-     nodePtr = nodePtr->next;
-     if(nodePtr != NULL)
-         printf("-->");
-  }
-  printf("\n");
+/* Return top element */
+int topelement()
+{
+    return(top->info);
 }
  
-void main() {
-   /* Initialize Stack */
-   initialize();
-   /* Push Elements in stack */
-   push(1);
-   push(2);
-   push(3);
-   push(4);
-   /* Prints Size of Stack */
-   printf("Stack Size : %d\n", getStackSize(top));
-   /* Printing top element of Stack */
-   printf("\nTop Element : %d\n", peek());
-   /* Printing Stack */
-   printf("Stack as linked List\n");
-   printStack(top);
-   /* Removing elements from stack */
-   pop();
-   pop();
-   pop();
-   pop();
-   pop();
-   printStack(top);
-    
-   return;
+/* Check if stack is empty or not */
+void empty()
+{
+    if (top == NULL)
+        printf("\n Stack is empty");
+    else
+        printf("\n Stack is not empty with %d elements", count);
+}
+ 
+/* Destroy entire stack */
+void destroy()
+{
+    top1 = top;
+ 
+    while (top1 != NULL)
+    {
+        top1 = top->ptr;
+        free(top);
+        top = top1;
+        top1 = top1->ptr;
+    }
+    free(top1);
+    top = NULL;
+ 
+    printf("\n All stack elements destroyed");
+    count = 0;
 }
